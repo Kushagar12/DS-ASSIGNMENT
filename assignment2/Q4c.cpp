777489d668c71ff1739a02adc5945c04cf63f32f
Kushagar12 <kpathania_be24@thapar.edu>
2025-08-12 23:51:58 +0530

    Q4c.cpp

--- assignment2/Q4c.cpp
+++ assignment2/Q4c.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-// Function to check if a character is a vowel
+
 bool isVowel(char ch) {
     ch = tolower(ch);
     return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
@@ -14,7 +14,7 @@ int main() {
     cout << "Enter a string: ";
     getline(cin, str);
 
-    // Iterate over each character and append if not vowel
+    
     for (char ch : str) {
         if (!isVowel(ch)) {
             result += ch;
@@ -25,3 +25,4 @@ int main() {
 
     return 0;
 }
+
