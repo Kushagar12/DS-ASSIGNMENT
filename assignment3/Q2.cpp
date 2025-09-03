d2a4fea6c3f44b9cad3761a69c894ed8487121ee
Kushagar12 <kpathania_be24@thapar.edu>
2025-09-03 13:20:09 +0530

    Q2.cpp

--- assignment3/Q2.cpp
+++ assignment3/Q2.cpp
@@ -1,57 +1,33 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
-#define MAX 100
+string reverseString(string str) {
+    stack<char> s;
 
-class Stack {
-    char arr[MAX];
-    int top;
-
-public:
-    Stack() {
-        top = -1;
+    
+    for (char ch : str) {
+        s.push(ch);
     }
 
-    bool isFull() {
-        return top == MAX - 1;
+
+    string reversed = "";
+    while (!s.empty()) {
+        reversed += s.top();
+        s.pop();
     }
 
-    bool isEmpty() {
-        return top == -1;
-    }
-
-    void push(char c) {
-        if (!isFull()) {
-            arr[++top] = c;
-        }
-    }
-
-    char pop() {
-        if (!isEmpty()) {
-            return arr[top--];
-        }
-        return '\0';  
-    }
-};
+    return reversed;
+}
 
 int main() {
     string input;
-    cout << "Enter the string to reverse: ";
+    cout << "Enter a string: ";
     getline(cin, input);
 
-    Stack s;
-
-    
-    for (char c : input) {
-        s.push(c);
-    }
-
-    
-    cout << "Reversed string: ";
-    while (!s.isEmpty()) {
-        cout << s.pop();
-    }
-    cout << endl;
+    string reversed = reverseString(input);
+    cout << "Reversed string: " << reversed << endl;
 
     return 0;
 }
+
